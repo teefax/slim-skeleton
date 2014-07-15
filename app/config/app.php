a2d3f54d317a6509c211e06458420d4e487fb867
Eike Bernhardt <ebe@cleverreach.com>
2014-07-15 10:32:25 +0200

    initial commit

---
+++ app/config/app.php
@@ -0,0 +1,90 @@
+<?php
+
+ref::config('showPrivateMembers', true);
+ref::config('expLvl', 3);
+ref::config('showUrls', false);
+
+// init app
+$app = new \SlimController\Slim(array(
+    'templates.path'             => dirname(__DIR__) . '/views',
+    'cookies.secret_key'         => md5('appsecretkey'),
+
+    'controller.class_prefix'    => '',
+    'controller.method_suffix'   => '',
+    'controller.template_suffix' => 'twig',
+
+    'mode'                       => 'production',
+    'debug'                      => true
+));
+
+
+if ($app->config('debug') === true) {
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+    $app->config('whoops.editor', 'sublime');
+    $app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
+}
+
+
+/** Create monolog logger and store logger in container as singleton
+ * (Singleton resources retrieve the same log resource definition each time)
+ *  $app->log->info("Log error");
+ *  try catch hook
+ *  $app->applyHook('log.request.info', $e->getMessage());
+ *  $app->log->error($e);
+ */
+$app->container->singleton('log', function () use ($app) {
+
+    $logpath = APP_PATH.'/app/logs/'.date('Y/m');
+    $logfile = $logpath.'/'.date('d').'.log';
+
+    $old = umask(0);
+    if (!is_dir($logpath)){ mkdir($logpath, 0777, true); }
+    if (!is_writable($logpath)){ chmod($logfile, 0777); }
+    if (!file_exists($logfile)){ file_put_contents($logfile, ''); }
+    umask($old);
+
+    $log = new \Monolog\Logger(strtoupper($app->request->getHost()));
+    $log->pushHandler(new \Monolog\Handler\StreamHandler($logfile, \Monolog\Logger::DEBUG, true, 0777));
+
+    return $log;
+});
+
+/**
+ * Register handlers
+ */
+\Monolog\ErrorHandler::register($app->log);
+
+/**
+ * Config Views
+ */
+$app->view(new \Slim\Views\Twig());
+$app->view->parserOptions = array(
+    'debug'            => true,
+    'charset'          => 'utf-8',
+    'cache'            => dirname(__DIR__).'/views/cache',
+    'auto_reload'      => true,
+    'strict_variables' => false,
+    'autoescape'       => true
+);
+$app->view->parserExtensions = array(new \Slim\Views\TwigExtension(), new Twig_Extension_Debug());
+$app->view->appendData(array( 'app' => $app ));
+
+/**
+ * Require all models
+ */
+foreach (glob(dirname(__DIR__)."/models/*.php") as $filename) {
+    require $filename;
+}
+
+/**
+ * Require all controllers
+ */
+foreach (glob(dirname(__DIR__)."/controllers/*.php") as $filename) {
+    require $filename;
+}
+
+require dirname(__DIR__)."/hooks.php";
+require dirname(__DIR__)."/routes.php";
+
+$app->run();
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// define a working directory
+define('APP_PATH', dirname(__DIR__)); // PHP v5.3+
+// load
+require APP_PATH . '/vendor/autoload.php';
+require APP_PATH . "/app/config/db.php";
+require APP_PATH . '/app/config/app.php';
